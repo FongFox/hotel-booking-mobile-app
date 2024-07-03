408f55f687f3cd51491ae7e4eadd6097d38069d3
Kurolsim <107937656+Kurolsim@users.noreply.github.com>
2024-07-03 14:48:43 +0700

    Update ContactComponent.js (compose function)
    s

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
 import { Text } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
+import * as MailComposer from 'expo-mail-composer';
+import * as Animatable from 'react-native-animatable';
 
 class Contact extends Component {
     render() {
         return (
-            <Card>
-                <Card.Title>Contact Information</Card.Title>
-                <Card.Divider />
-                <Text style={{ margin: 10 }}>121, Clear Water Bay Road</Text>
-                <Text style={{ margin: 10 }}>Clear Water Bay, Kowloon</Text>
-                <Text style={{ margin: 10 }}>HONG KONG</Text>
-                <Text style={{ margin: 10 }}>Tel: +852 1234 5678</Text>
-                <Text style={{ margin: 10 }}>Fax: +852 8765 4321</Text>
-                <Text style={{ margin: 10 }}>Email: support@hotelbookingapp.com</Text>
-            </Card>
+            <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                <Card>
+                    <Card.Title>Contact Information</Card.Title>
+                    <Card.Divider />
+                    <Text style={{ margin: 10 }}>121, Clear Water Bay Road</Text>
+                    <Text style={{ margin: 10 }}>Clear Water Bay, Kowloon</Text>
+                    <Text style={{ margin: 10 }}>HONG KONG</Text>
+                    <Text style={{ margin: 10 }}>Tel: +852 1234 5678</Text>
+                    <Text style={{ margin: 10 }}>Fax: +852 8765 4321</Text>
+                    <Text style={{ margin: 10 }}>Email: support@hotelbookingapp.com</Text>
+                    <Button title=' Compose Email' buttonStyle={{ backgroundColor: '#7cc' }}
+                        icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                        onPress={this.composeMail} />
+                </Card>
+            </Animatable.View>
         );
+    }
+    composeMail() {
+        MailComposer.composeAsync({
+            recipients: ['<email_address>'],
+            subject: 'From me',
+            body: 'Dear Hotel ...'
+        });
     }
 }
 export default Contact;
